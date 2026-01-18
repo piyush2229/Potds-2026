e370d562c1b12821bd43760f643d972715f94e84
Piyush Kumar Upadhyaya <122392401+piyush2229@users.noreply.github.com>
2026-01-18 23:26:34 +0530

    Implement next frequency greater function

---
+++ Gfg/18-01-26.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> nextFreqGreater(vector<int>& arr) {
+
+        stack<int> st;
+        unordered_map<int,int> mp;
+        for(int& elem : arr){
+            mp[elem]++;
+        }
+        int n = arr.size();
+        vector<int> ans(n,-1);
+        for(int i = n-1;i>=0;i--){
+            if(!st.empty() &&   mp[arr[i]]<mp[st.top()]){
+                ans[i] = st.top();
+            }else{
+                while(!st.empty() && mp[arr[i]]>=mp[st.top()]){
+                    st.pop();
+                }
+                if(!st.empty()){
+                    ans[i] = st.top(); 
+                }
+            }
+            st.push(arr[i]);
+        }
+        
+        return ans;
+         
+    }
+};
